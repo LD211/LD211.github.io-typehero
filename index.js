21eb8843908705d694e5a5f1a4717f4410b5a968
lucas github <ld@ld.rip>
2023-05-18 19:32:14 -0500

    added eng200, live wpm

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let greenChar = "";
 let entireSong;
 let theAudio;
 let songIndex = 0;
+let liveWpmDisplayed = false;
 let title = "fallingdown";
 //document.body.style.backgroundImage = "url('./images/"+title+".webp')";
 const textInput = document.getElementById("textInput");
@@ -15,6 +16,7 @@ let startFlag = 0;
 let display = document.getElementById("typing");
 const typedText = document.createElement("span");
 display.appendChild(typedText);
+let liveWpm = document.getElementById("livewpm");
 const songNames = [
   "bands",
   "givemeareason",
@@ -33,6 +35,11 @@ getSongLyrics();
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     restartGame();
+    if (title === "english200") {
+      display.innerText = "";
+      getEnglishWords();
+      updateCursor();
+    }
   } else if (event.key === "ArrowLeft") {
     songIndex--;
     if (songIndex < 0) {
@@ -44,6 +51,12 @@ document.addEventListener("keydown", (event) => {
     getSongLyrics();
     restartGame();
     updateCursor();
+  } else if (event.key === "&") {
+    title = "english200";
+    display.innerText = "";
+    getEnglishWords();
+    spotNumber = 0;
+    updateCursor();
   }
 });
 
@@ -51,11 +64,40 @@ display.addEventListener("click", function () {
   textInput.focus();
 });
 
+function liveWordsPerMinute() {
+  setInterval(function () {
+    let endTimes = new Date();
+    let secondslive = (endTimes.getTime() - startTime.getTime()) / 1000;
+
+    liveWpm.innerText = Math.floor((spotNumber / 5) / secondslive * 60) +
+      " wpm";
+  }, 1000);
+}
+
 function restartGame() {
-  theAudio.pause();
+  if (theAudio) theAudio.pause();
   startFlag = 0;
   spotNumber = 0;
   updateCursor();
+}
+
+function getEnglishWords() {
+  fetch("./lyrics/" + title + ".txt")
+    .then((response) => {
+      return response.text();
+    })
+    .then((text) => {
+      entireSong = text;
+      //entireSong.replaceAll('\r\n', '<br>');
+      //entireSong.replaceAll('\r', '<br>');
+      entireSong = entireSong.replace(/\r?\n|\r/g, "\n");
+      const mtWords = entireSong.split("\n");
+      for (let i = 0; i < mtWords.length; i++) {
+        mtWords[i] = mtWords[Math.floor(Math.random() * 198) + 1];
+      }
+      entireSong = mtWords.join(" ");
+      interpretWords(entireSong);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -63,11 +105,17 @@ document.addEventListener("DOMContentLoaded", function () {
   cursor.style.left = "10px";
   cursor.style.top = "35px";
   textInput.addEventListener("input", (event) => {
+    if (liveWpmDisplayed === false) {
+      liveWordsPerMinute();
+      liveWpmDisplayed = true;
+    }
     if (startFlag === 0) {
       startTime = new Date();
       if (title) {
         theAudio = new Audio("./sounds/" + title + ".mp3");
-        visualizeTheAudio(theAudio);
+        if (theAudio) {
+          visualizeTheAudio(theAudio);
+        }
         //theAudio.play()
         startFlag = 1;
       } else {
